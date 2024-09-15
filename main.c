0d82015a85e16a0bb6fff50f7c1aa41adee94d5a
NikitaArote_git <arotenikita2002@gmail.com>
2024-09-15 23:13:31 +0530

    Updated main.c with new code

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_BOOKS 100
+#define MAX_TITLE 100
+#define MAX_AUTHOR 100
+
+typedef struct {
+    int id;
+    char title[MAX_TITLE];
+    char author[MAX_AUTHOR];
+    int is_available;
+} Book;
+
+Book library[MAX_BOOKS];
+int book_count = 0;
